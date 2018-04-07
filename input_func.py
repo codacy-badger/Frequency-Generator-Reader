524538a5fd126d779ff343f7abca7683a09fda04
David Gurevich <david.gurevich111@gmail.com>
2018-04-07 16:07:19 -0400

    Update names of files.

--- input_func.py
+++ input_func.py
@@ -5,6 +5,7 @@
  **Author:** David Gurevich                                                                                           \n
  **Required Modules:**
        * matplotlib                                                                                                   \n
+       * mcculw                                                                                                       \n
 
 This work is licensed under the Creative Commons Attribution-NonCommercial-NoDerivs 3.0 Unported License.
 To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-nd/3.0/ or send a letter to
